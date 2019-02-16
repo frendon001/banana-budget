3845468e5ab25f74e9559de4c88d9194980b8112
Fausto Rendon <faustor001@gmail.com>
2019-02-16 15:16:24 -0800

    Add API route to calculate totalCost for Banana Budget

---
+++ server/routes/expenses.js
@@ -0,0 +1,19 @@
+const Path = require('path-parser').default;
+const { URL } = require('url');
+const BananaBudget = require('../services/BananaBudget');
+
+module.exports = app => {
+  //Route to receive banana budget calculation request
+  app.get('/api/bananaBudget', (req, res) => {
+    const budget = new BananaBudget(req.query);
+    //validate request and send response
+    if(!budget.showHasValidParams()){
+      res.status(400).send(budget.showErrorMessage());
+    } else {
+      res.status(200).send({
+        totalCost: budget.totalBananaExpenses()
+      });
+    }
+  });
+
+};
